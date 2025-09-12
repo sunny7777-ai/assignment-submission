22cc649ab8d79d3c4f89d60fc01de70f6b7d366a
Sunny <jie@gyodi.com>
2025-09-12 15:14:57 +1000

    remove all .sh files

--- question03.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a new file named file3.txt
-echo "This is file3.txt content." > file3.txt
-
-# Stage the file
-git add file3.txt
--- question14.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Fetch and switch to remote branch 'update1'
-git fetch origin update1
-git checkout update1
